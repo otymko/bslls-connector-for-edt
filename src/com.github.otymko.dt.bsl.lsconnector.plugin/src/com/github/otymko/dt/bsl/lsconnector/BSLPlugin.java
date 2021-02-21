799c7bd432a29f4e73ba9ebff832f89dcef2edc6
Tymko Oleg <olegtymko@yandex.ru>
2021-02-21 10:12:30 +0700

    refactor: чистка

--- src/com.github.otymko.dt.bsl.lsconnector.plugin/src/com/github/otymko/dt/bsl/lsconnector/BSLPlugin.java
+++ src/com.github.otymko.dt.bsl.lsconnector.plugin/src/com/github/otymko/dt/bsl/lsconnector/BSLPlugin.java
@@ -1,51 +1,17 @@
 package com.github.otymko.dt.bsl.lsconnector;
 
-import java.io.IOException;
-import java.nio.file.Path;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Optional;
-import java.util.Set;
-
-import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Plugin;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.preferences.InstanceScope;
-import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.preferences.ScopedPreferenceStore;
 import org.osgi.framework.BundleContext;
-
-import com.github.otymko.dt.bsl.lsconnector.listener.WindowEventListener;
-import com.github.otymko.dt.bsl.lsconnector.service.LSService;
-import com.github.otymko.dt.bsl.lsconnector.service.WindowsEventService;
-import com.github.otymko.dt.bsl.lsconnector.ui.BSLPreferencePage;
-import com.github.otymko.dt.bsl.lsconnector.util.BSLCommon;
 
 import lombok.Getter;
 
 public class BSLPlugin extends Plugin {
-    public static final String PLUGIN_ID = "com.github.otymko.dt.bsl.ls_connector";
-    @Getter
-    private Set<String> workbenchParts = Collections.synchronizedSet(new HashSet<>());
+    public static final String PLUGIN_ID = "com.github.otymko.dt.bsl.lsconnector";
     @Getter
     private static BSLPlugin plugin;
-    @Getter
-    protected static BundleContext context;
-    @Getter
-    private WindowsEventService windowsEventService;
-    @Getter
-    private LSService lsService;
-    @Getter
-    private Path appDir;
-    @Getter
-    private Path pathToImageApp;
-    @Getter
-    private Path pathToWorkspace;
-    @Getter
-    private Optional<Path> pathToConfiguration;
-    @Getter
-    private ScopedPreferenceStore preferenceStore;
+    
 
     public static IStatus createErrorStatus(String message, Throwable throwable) {
 	return new Status(IStatus.ERROR, PLUGIN_ID, 0, message, throwable);
@@ -61,100 +27,13 @@ public class BSLPlugin extends Plugin {
 
     @Override
     public void start(BundleContext bundleContext) throws Exception {
-	plugin = this;
 	super.start(bundleContext);
-	BSLPlugin.context = bundleContext;
-
-	initialize();
-	startServices();
-	startLS();
     }
 
     @Override
     public void stop(BundleContext bundleContext) throws Exception {
-	stopLS();
 	plugin = null;
-
-	if (PlatformUI.isWorkbenchRunning()) {
-	    for (var window : PlatformUI.getWorkbench().getWorkbenchWindows()) {
-		WindowEventListener.removeListenerFromAllPages(window);
-	    }
-	}
-
 	super.stop(bundleContext);
     }
-
-    public void sleepCurrentThread(long value) {
-	try {
-	    Thread.sleep(value);
-	} catch (Exception e) {
-	    createWarningStatus(e.getMessage());
-	}
-    }
-
-    private void startLS() {
-	BSLCommon.downloadBSLLS(pathToImageApp);
-	lsService.start();
-    }
-
-    private void stopLS() {
-	lsService.stop();
-    }
-
-    public void restartLS() {
-	lsService.restart();
-    }
-
-    public boolean isRunningLS() {
-	return lsService.isLaunched();
-    }
-
-    private void initialize() {
-	initAppDir();
-	initPreferenceStore();
-	prepareForStart();
-    }
-
-    private void startServices() {
-	windowsEventService = new WindowsEventService();
-	lsService = new LSService(this);
-    }
-
-    private void initPreferenceStore() {
-	preferenceStore = new ScopedPreferenceStore(InstanceScope.INSTANCE, PLUGIN_ID);
-	preferenceStore.setDefault(BSLPreferencePage.PATH_TO_BSLLS, plugin.getPathToImageApp().toString());
-	preferenceStore.setDefault(BSLPreferencePage.EXTERNAL_JAR, false);
-	preferenceStore.setDefault(BSLPreferencePage.PATH_TO_JAVA, "java");
-	preferenceStore.setDefault(BSLPreferencePage.JAVA_OPTS, "");
-    }
-
-    private void prepareForStart() {
-	pathToWorkspace = ResourcesPlugin.getWorkspace().getRoot().getLocation().toFile().toPath();
-
-	try {
-	    searchConfigurationFile();
-	} catch (IOException e) {
-	    createErrorStatus(e.getMessage(), e);
-	}
-    }
-
-    private void searchConfigurationFile() throws IOException {
-	pathToConfiguration = BSLCommon.getConfigurationFileFromWorkspace(pathToWorkspace);
-    }
-
-    private void initAppDir() {
-	appDir = Path.of(System.getProperty("user.home"), ".bsl-connector-for-edt");
-	if (!appDir.toFile().exists()) {
-	    appDir.toFile().mkdir();
-	}
-
-	// проверим есть ли image app BSL LS
-	var pathToApp = Path.of(appDir.toString(), "bsl-language-server").toFile();
-	if (!pathToApp.exists()) {
-	    pathToApp.mkdir();
-	}
-
-	// путь к image app
-	pathToImageApp = Path.of(pathToApp.toString(), "bsl-language-server.exe");
-    }
+    
 }
--- src/com.github.otymko.dt.bsl.lsconnector.plugin/src/com/github/otymko/dt/bsl/lsconnector/BSLValidator.java
+++ src/com.github.otymko.dt.bsl.lsconnector.plugin/src/com/github/otymko/dt/bsl/lsconnector/BSLValidator.java
@@ -1,23 +1,13 @@
 package com.github.otymko.dt.bsl.lsconnector;
 
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.util.EcoreUtil;
-import org.eclipse.jface.text.BadLocationException;
-import org.eclipse.jface.text.Document;
-import org.eclipse.lsp4j.Diagnostic;
-import org.eclipse.lsp4j.DiagnosticSeverity;
-import org.eclipse.xtext.nodemodel.util.NodeModelUtils;
 import org.eclipse.xtext.util.CancelIndicator;
 import org.eclipse.xtext.validation.Check;
 import org.eclipse.xtext.validation.CheckType;
 
 import com._1c.g5.v8.dt.bsl.model.Module;
-import com._1c.g5.v8.dt.bsl.resource.BslResource;
 import com._1c.g5.v8.dt.bsl.validation.CustomValidationMessageAcceptor;
 import com._1c.g5.v8.dt.bsl.validation.IExternalBslValidator;
-import com.github.otymko.dt.bsl.lsconnector.util.BSLCommon;
 
 public class BSLValidator implements IExternalBslValidator {
 
@@ -29,93 +19,6 @@ public class BSLValidator implements IExternalBslValidator {
     @Override
     @Check(CheckType.EXPENSIVE)
     public void validate(EObject object, CustomValidationMessageAcceptor messageAcceptor, CancelIndicator monitor) {
-	// попытка прервать
-	if (monitor.isCanceled()) {
-	    return;
-	}
-
-	var resource = (BslResource) ((Module) object).eResource();
-	if (!resource.isDeepAnalysing()) {
-	    return;
-	}
-
-	BSLPlugin.getPlugin().getLsService().getConnector()
-		.runFutureTask(() -> validateFuture(object, messageAcceptor, monitor));
-    }
-
-    private void validateFuture(EObject object, CustomValidationMessageAcceptor messageAcceptor,
-	    CancelIndicator monitor) {
-	var plugin = BSLPlugin.getPlugin();
-	var connector = plugin.getLsService().getConnector();
-
-	if (!plugin.isRunningLS()) {
-	    return;
-	}
-
-	var module = (Module) object;
-	var node = NodeModelUtils.findActualNodeFor(module);
-	var content = node.getText();
-	if (content == null) {
-	    content = "";
-	}
-	var document = new Document(content);
-	var moduleFile = ResourcesPlugin.getWorkspace().getRoot()
-		.getFile(new Path(EcoreUtil.getURI(module).toPlatformString(true)));
-	var uri = BSLCommon.uri(moduleFile.getLocationURI());
-
-	// Костыль при открытии, если на форме нет фокуса
-	if (plugin.getWorkbenchParts().contains(uri.toString())) {
-	    connector.textDocumentDidChange(uri, content);
-	} else {
-	    plugin.getWorkbenchParts().add(uri.toString());
-	    connector.textDocumentDidOpen(uri, content);
-	}
-
-	// FIXME: иначе может быть NPE
-	plugin.sleepCurrentThread(1000);
-	// попытка прервать
-	if (monitor.isCanceled()) {
-	    return;
-	}
-	// если документ уже закрыт
-	if (!plugin.getWorkbenchParts().contains(uri.toString())) {
-	    return;
-	}
-
-	var diagnostics = connector.diagnostics(uri.toString());
-
-	// попытка прервать
-	if (monitor.isCanceled()) {
-	    return;
-	}
-
-	diagnostics.forEach(diagnostic -> {
-	    // попытка прервать
-	    if (monitor.isCanceled()) {
-		return;
-	    }
-	    acceptIssue(module, messageAcceptor, diagnostic, document);
-	});
-    }
-
-    private void acceptIssue(Module module, CustomValidationMessageAcceptor messageAcceptor, Diagnostic diagnostic,
-	    Document document) {
-	int[] offsetParams;
-	try {
-	    offsetParams = BSLCommon.getOffsetByRange(diagnostic.getRange(), document);
-	} catch (BadLocationException e) {
-	    BSLPlugin.createErrorStatus(e.getMessage(), e);
-	    return;
-	}
-	var severity = diagnostic.getSeverity();
-	var message = "[BSL LS] " + diagnostic.getMessage();
-	if (severity == DiagnosticSeverity.Error) {
-	    messageAcceptor.acceptError(message, module, offsetParams[0], offsetParams[1], diagnostic.getCode());
-	} else if (severity == DiagnosticSeverity.Warning) {
-	    messageAcceptor.acceptWarning(message, module, offsetParams[0], offsetParams[1], diagnostic.getCode());
-	} else {
-	    messageAcceptor.acceptInfo(message, module, offsetParams[0], offsetParams[1], diagnostic.getCode());
-	}
 
     }
 
