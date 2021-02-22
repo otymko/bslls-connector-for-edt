2d0e13296a7d2349819a486f9e7265e50938e01a
Tymko Oleg <olegtymko@yandex.ru>
2021-02-22 17:32:55 +0700

    Промежуточный результат (много работы)

--- src/com.github.otymko.dt.bsl.lsconnector.plugin/src/com/github/otymko/dt/bsl/lsconnector/BSLValidator.java
+++ src/com.github.otymko.dt.bsl.lsconnector.plugin/src/com/github/otymko/dt/bsl/lsconnector/BSLValidator.java
@@ -1,6 +1,7 @@
 package com.github.otymko.dt.bsl.lsconnector;
 
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.jface.text.Document;
 import org.eclipse.xtext.util.CancelIndicator;
 import org.eclipse.xtext.validation.Check;
 import org.eclipse.xtext.validation.CheckType;
@@ -8,6 +9,9 @@ import org.eclipse.xtext.validation.CheckType;
 import com._1c.g5.v8.dt.bsl.model.Module;
 import com._1c.g5.v8.dt.bsl.validation.CustomValidationMessageAcceptor;
 import com._1c.g5.v8.dt.bsl.validation.IExternalBslValidator;
+import com.github.otymko.dt.bsl.lsconnector.core.BSLCore;
+import com.github.otymko.dt.bsl.lsconnector.util.BSLCommon;
+import com.github.otymko.dt.bsl.lsconnector.util.ValidatorHelper;
 
 public class BSLValidator implements IExternalBslValidator {
 
@@ -18,8 +22,52 @@ public class BSLValidator implements IExternalBslValidator {
 
     @Override
     @Check(CheckType.EXPENSIVE)
-    public void validate(EObject object, CustomValidationMessageAcceptor messageAcceptor, CancelIndicator monitor) {
-
+    public void validate(EObject object, CustomValidationMessageAcceptor messageAcceptor, 
+	    CancelIndicator monitor) {
+	
+	if (monitor.isCanceled()) {
+	    return;
+	}
+	
+	var module = (Module) object;
+	if (!ValidatorHelper.isDeepAnalysing(module)) {
+	    return;
+	}
+	
+	BSLCommon.runFutureTask(
+		() -> validateFuture(module, messageAcceptor, monitor), BSLCommon.DEFAULT_TIMEOUT);
+    }
+    
+    private void validateFuture(Module module, CustomValidationMessageAcceptor messageAcceptor, 
+	    CancelIndicator monitor) {
+	
+	var connector = ValidatorHelper.getBSLConnector(module);
+	if (connector == null) {
+	    BSLActivator.createWarningStatus("Нет подключения к BSL LS");
+	    return;
+	}
+	
+	var content = ValidatorHelper.getContentFromModule(module);
+	var document = new Document(content);
+	var uri = ValidatorHelper.getUriFromModule(module);
+	
+	// или DidOpen или DidChange
+	connector.textDocumentDidOpen(uri, content);
+	// нужно ждать textDocumentDidOpen
+	BSLCore.getInstance().sleepCurrentThread(1000);
+	
+	if (monitor.isCanceled()) {
+	    return;
+	}
+	
+	var diagnostics = connector.diagnostics(uri.toString());
+	
+	diagnostics.forEach(diagnostic -> {
+	    if (monitor.isCanceled()) {
+		return;
+	    }
+	    ValidatorHelper.publishIssue(module, messageAcceptor, diagnostic, document);
+	});
     }
 
 }
---
+++ src/com.github.otymko.dt.bsl.lsconnector.plugin/src/com/github/otymko/dt/bsl/lsconnector/service/LanguageService.java
@@ -0,0 +1,124 @@
+package com.github.otymko.dt.bsl.lsconnector.service;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.resources.ResourcesPlugin;
+
+import com.github.otymko.dt.bsl.lsconnector.BSLActivator;
+import com.github.otymko.dt.bsl.lsconnector.core.BSLCore;
+import com.github.otymko.dt.bsl.lsconnector.core.PluginSetting;
+import com.github.otymko.dt.bsl.lsconnector.lsp.BSLConnector;
+import com.github.otymko.dt.bsl.lsconnector.lsp.BSLLanguageClient;
+
+import lombok.Getter;
+
+public class LanguageService {
+    /**
+     * Глобальные настройки плагина
+     */
+    private final PluginSetting setting;
+    
+    /**
+     * Путь к базовому каталогу проекта
+     */
+    private final Path rootPath;
+    
+    private Process process;
+    @Getter
+    private BSLConnector connector;
+  
+    public LanguageService(PluginSetting setting, Path rootPath) {
+	this.setting = setting;
+	this.rootPath = rootPath;
+    }
+    
+    public void start() {
+	createProcess();
+	connectToProcess();
+    }
+    
+    public void stop() {
+	if (connector != null) {
+	    connector.shutdown();
+	    connector.exit();
+	}
+	clear();
+    }
+    
+    public void restart() {
+	stop();
+	start();
+    }
+    
+    public boolean isLaunched() {
+	return process != null && process.isAlive();
+    }
+    
+    private void createProcess() {
+	var pathToWorkspace = ResourcesPlugin.getWorkspace().getRoot().getLocation().toFile().toPath();
+	var externalJar = false;
+	var isImageApp = !externalJar;
+	var pathToLSP = getPathToBSLLS();
+
+	if (!pathToLSP.toFile().exists()) {
+	    return;
+	}
+
+	List<String> arguments = new ArrayList<>();
+	if (!isImageApp) {
+//	    arguments.add(preferenceStore.getString(BSLPreferencePage.PATH_TO_JAVA));
+	    arguments.add("-jar");
+	}
+	arguments.add("\"" + pathToLSP.toString() + "\"");
+	
+	if (isImageApp) {
+	    arguments.add("lsp");
+	}
+
+//	var pathToConfiguration = Path.of("D:\\DATA\\Develop\\runtime-New_configuration\\.bsl-language-server.json");
+////	if (pathToConfiguration.isPresent()) {
+//	    arguments.add("--configuration");
+//	    arguments.add(pathToConfiguration.toString());
+////	}
+
+	BSLActivator.createWarningStatus(arguments.toString());
+
+	try {
+	    process = new ProcessBuilder()
+		    .command(arguments)
+		    .directory(pathToWorkspace.toFile())
+		    .start();
+	    BSLCore.getInstance().sleepCurrentThread(500);
+	    if (!process.isAlive()) {
+		BSLActivator.createWarningStatus("Не удалалось запустить процесс с BSL LS. Процесс был аварийно завершен.");
+	    }
+	} catch (IOException e) {
+	    BSLActivator.createWarningStatus("Не удалось запустить процесс BSL LS", e);
+	}
+    }
+    
+    private void connectToProcess() {
+	if (process == null) {
+	    return;
+	}
+	var client = new BSLLanguageClient();
+	connector = new BSLConnector(client, process.getInputStream(), process.getOutputStream(), rootPath);
+//	connector.setPathToRoot(ResourcesPlugin.getWorkspace().getRoot().getLocation().toFile().toPath());
+	connector.startInThread();
+	BSLCore.getInstance().sleepCurrentThread(2000); // FIXME: Сколько нужно ждать?
+	connector.initialize();
+    }
+    
+    private void clear() {
+	process = null; 
+	connector = null;
+    }
+    
+    private Path getPathToBSLLS() {
+	return setting.getPathToLS();
+//	return Path.of(Optional.of(preferenceStore.getString(BSLPreferencePage.PATH_TO_BSLLS)).orElse(""));
+    }
+}
